aa3b17aaf4a1ccf0855d360eb47ed37a237c82d0
fwindpeak <fwindpeak@gmail.com>
2020-12-24 00:19:53 +0800

    add filter

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
 <template>
   <div id="app">
-    <div v-for="item of allData" :key="item.node.id">
+    <h1>Oculus Quest 游戏列表</h1>
+    <div>
+      <span>筛选:</span><input v-model="keywords" class="input" />
+      <span><input type="checkbox" v-model="isFree" value="free" />免费</span>
+      <span
+        ><input
+          type="checkbox"
+          v-model="isDiscount"
+          value="discount"
+        />打折</span
+      >
+    </div>
+    <div v-for="item of showData" :key="item.node.id">
       <Card :nodeData="item.node"></Card>
     </div>
   </div>
@@ -18,7 +30,27 @@ export default {
   data() {
     return {
       allData: [],
+      keywords: "",
+      isFree: false,
+      isDiscount: true,
     };
+  },
+  computed: {
+    showData() {
+      return this.allData.filter((item) => {
+        if (this.isDiscount && !item.node.current_offer.promo_benefit) {
+          return false;
+        }
+        if (
+          this.isFree &&
+          item.node.current_offer.price.formatted !== "$0.00"
+        ) {
+          return false;
+        }
+        const name = item.node.display_name.toLowerCase();
+        return name.indexOf(this.keywords.toLowerCase()) != -1;
+      });
+    },
   },
   created() {
     getStoreData().then((res) => {
@@ -36,6 +68,21 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 5px;
+}
+.input {
+  max-width: 100%;
+  outline: 0px;
+  position: relative;
+  appearance: none;
+  transition: all 0.2s ease 0s;
+  font-size: 1rem;
+  padding-left: 1rem;
+  padding-right: 1rem;
+  height: 2.5rem;
+  border-radius: 0.375rem;
+  border-width: 1px;
+  border-style: solid;
+  border-color: rgb(226, 232, 240);
 }
 </style>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,8 +1,8 @@
 <template>
-  <div>
-    <img :src="getCoverUrl(nodeData.id)" />
-    <h1>{{ nodeData.display_name }}</h1>
+  <div class="card" @click="onCardClicked(nodeData.id)">
+    <h2>{{ nodeData.display_name }}</h2>
     <Price :current_offer="nodeData.current_offer"></Price>
+    <img :src="getCoverUrl(nodeData.id)" class="img" />
   </div>
 </template>
 
@@ -19,9 +19,23 @@ export default {
     getCoverUrl(id) {
       return `/cover/${id}/landscape.jpg`;
     },
+    onCardClicked(id) {
+      window.open(`https://www.oculus.com/experiences/quest/${id}/`);
+    },
   },
 };
 </script>
 
 <style>
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+  border-radius: 5px;
+  margin: 5px;
+  padding: 5px;
+}
+.img {
+  border-radius: 5px;
+  max-width: 100%;
+}
 </style>
