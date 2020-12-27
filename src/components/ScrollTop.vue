2fcee02ee62b6446189531c5ad320905c4a4ad33
fwindpeak <fwindpeak@gmail.com>
2020-12-27 13:27:01 +0800

    update

--- src/components/ScrollTop.vue
+++ src/components/ScrollTop.vue
@@ -1,10 +1,5 @@
 <template>
-  <!--transition标签 按钮出现附带动画-->
   <transition name="el-fade-in">
-    <!-- <div class="page-component-up" @click="scrollToTop" v-show="true"> -->
-    <!-- <i class="el-icon-caret-top" @click="handleScroll"></i> -->
-    <!-- <van-button icon="plus" type="primary" /> -->
-    <!-- </div> -->
     <div class="page-component-up" @click="scrollToTop" v-show="true">
       <van-icon class="el-icon-caret-top" name="back-top"></van-icon>
     </div>
@@ -18,61 +13,12 @@ export default {
       toTopShow: false,
     };
   },
-  computed: {
-    scrollSize() {
-      return (
-        document.body.scrollTop ||
-        document.documentElement.scrollTop ||
-        window.pageYoffset
-      );
-    },
-  },
   methods: {
-    handleScroll() {
-      // console.log("handleScroll");
-      // //id scroller-box是自己在组件上添加的，为了方便获取dom
-      // this.scrollTop = document.documentElement.scrollTop;
-      // if (this.scrollTop > 300) {
-      //   this.toTopShow = true;
-      // } else {
-      //   this.toTopShow = false;
-      // }
-    },
     scrollToTop() {
-      console.log("scroll to top");
       document.documentElement.scrollTop = 0;
       document.body.scrollTop = 0;
       document.getElementById("scroller-box").children[0].scrollTop = 0;
-      // let timer = null,
-      //   _that = this;
-      // //动画，使用requestAnimationFrame代替setInterval
-      // cancelAnimationFrame(timer);
-      // timer = requestAnimationFrame(function fn() {
-      //   if (_that.scrollTop > 0) {
-      //     _that.scrollTop -= 50;
-      //     document.documentElement.scrollTop = _that.scrollTop;
-      //     //   document.getElementById("scroller-box").scrollTop = 0;
-      //     document.body.scrollTop = 0;
-      //     timer = requestAnimationFrame(fn);
-      //   } else {
-      //     cancelAnimationFrame(timer);
-      //     _that.toTopShow = false;
-      //   }
-      // });
     },
-  },
-  mounted() {
-    // //$nextTick 避免dom未加载
-    // this.$nextTick(function () {
-    //   let targetScroll = document.body;
-    //   console.log("targetScroll", targetScroll);
-    //   console.log("scrollSize", this.scrollSize);
-    //   targetScroll.addEventListener("scroll", this.handleScroll);
-    // });
-  },
-  destroyed() {
-    // let targetScroll = document.getElementById("scroller-box");
-    // targetScroll.removeEventListener("scroll", this.handleScroll);
   },
 };
 </script>
